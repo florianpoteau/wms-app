befe1a3fb401e7dfff780ffd8ff83ecec02a767f
Poteau Florian <florianpoteau59@gmail.com>
2026-08-10 18:21:12 +0200

    feat(auth): add login service

---
+++ backend/services/auth/login.service.ts
@@ -0,0 +1,49 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import type { UserloginInput } from "../../validators/auth/login.validator";
+import { findUserByEmail } from "../../repositories/users/login.repository";
+
+export async function loginUserService(data: UserloginInput) {
+  const user = await findUserByEmail(data.email);
+
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!user) {
+    throw new Error("INVALID_CREDENTIALS");
+  }
+
+  const passwordValid = await bcrypt.compare(
+    data.password,
+    user.passwordhashed,
+  );
+
+  if (!passwordValid) {
+    throw new Error("INVALID_CREDENTIALS");
+  }
+
+  if (!jwtSecret) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+      role: user.role,
+    },
+    jwtSecret,
+    {
+      expiresIn: "1h",
+    },
+  );
+
+  return {
+    token,
+    user: {
+      id: user.id,
+      email: user.email,
+      firstname: user.firstname,
+      name: user.name,
+      role: user.role,
+    },
+  };
+}
