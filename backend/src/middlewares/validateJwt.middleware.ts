3e8ce2289118b1656a73141773dd9322e0bfb450
Poteau Florian <florianpoteau59@gmail.com>
2026-08-11 17:45:04 +0200

    feat(auth): add JWT authentication middleware

---
+++ backend/src/middlewares/validateJwt.middleware.ts
@@ -0,0 +1,36 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
+export interface AuthRequest extends JwtPayload {
+  userId?: string;
+  role?: string;
+}
+
+export const authMiddleware = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authorizationHeader = req.header("Authorization");
+
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid authorization header" });
+  }
+
+  const token = authorizationHeader.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token not found" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    req.token = decoded;
+    next();
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
