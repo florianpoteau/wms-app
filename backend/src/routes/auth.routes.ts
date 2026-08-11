6a55d3410846da88904b1ec7abd220a73c2adf9b
Poteau Florian <florianpoteau59@gmail.com>
2026-08-11 17:46:50 +0200

    feat(auth): add controller, service and routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { loginController } from "../controllers/auth/login.controller";
 import { validate } from "../middlewares/validate.middleware";
 import { userloginSchema } from "../validators/auth/login.validator";
+import { meController } from "../controllers/auth/me.controller";
+import { authMiddleware } from "../middlewares/validateJwt.middleware";
 
 const router = Router();
 
 router.post("/auth/login", validate(userloginSchema), loginController);
+router.get("/auth/me", authMiddleware, meController);
 
 export default router;
