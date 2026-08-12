b32318ca6c27f625e18c342b0c1636a52ec50b96
Poteau Florian <florianpoteau59@gmail.com>
2026-08-12 17:14:36 +0200

    feat(auth): store JWT token in cookie

--- backend/src/controllers/auth/login.controller.ts
+++ backend/src/controllers/auth/login.controller.ts
@@ -4,5 +4,10 @@ import type { Request, Response } from "express";
 export const loginController = async (req: Request, res: Response) => {
   const result = await loginUserService(req.body);
 
+  res.cookie("accessToken", result.token, {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+  });
+
   return res.status(200).json(result);
 };
