cc4ad26bed3e647696dc6f0305b3a45fcd736b3f
Poteau Florian <florianpoteau59@gmail.com>
2026-08-14 18:10:13 +0200

    feat(login): add proxy and tanstack query

--- frontend/src/forms/login.schema.tsx
+++ frontend/src/forms/login.schema.tsx
@@ -9,15 +9,9 @@ export const loginSchema = z.object({
         .string()
         .min(8, 'Le mot de passe doit contenir au moins 8 caractères')
         .trim()
-        .regex(/[A-Z]/, {
-            error: "Le mot de passe doit contenir au moins une majuscule",
-        })
         .regex(/[0-9]/, {
             error: "Le mot de passe doit contenir au moins un chiffre",
         })
-        .regex(/[^A-Za-z0-9]/, {
-            error: "Le mot de passe doit contenir au moins un caractère spécial",
-        }),
 })
 
 export type loginFormData = z.infer<typeof loginSchema>
