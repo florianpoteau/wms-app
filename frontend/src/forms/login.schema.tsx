141f81098a7a5eb4dd103f47051419fddedc6d38
Poteau Florian <florianpoteau59@gmail.com>
2026-08-14 17:25:15 +0200

    feat(login): add password regex validation

--- frontend/src/forms/login.schema.tsx
+++ frontend/src/forms/login.schema.tsx
@@ -2,12 +2,22 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
     email: z
-        .string()
-        .email('Adresse email invalide'),
+        .email('Adresse email invalide')
+        .trim(),
 
     password: z
         .string()
-        .min(8, 'Le mot de passe doit contenir au moins 8 caractères'),
+        .min(8, 'Le mot de passe doit contenir au moins 8 caractères')
+        .trim()
+        .regex(/[A-Z]/, {
+            error: "Le mot de passe doit contenir au moins une majuscule",
+        })
+        .regex(/[0-9]/, {
+            error: "Le mot de passe doit contenir au moins un chiffre",
+        })
+        .regex(/[^A-Za-z0-9]/, {
+            error: "Le mot de passe doit contenir au moins un caractère spécial",
+        }),
 })
 
 export type loginFormData = z.infer<typeof loginSchema>
