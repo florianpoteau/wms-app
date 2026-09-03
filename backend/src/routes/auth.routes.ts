9c14027fa0b9a4ada76b8f37b5dcd9dcf39ae8e7
Poteau Florian <florianpoteau59@gmail.com>
2026-09-03 17:45:05 +0200

    refactor(routes): add controller class

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,7 +1,4 @@
 import { Router } from "express";
-import { meController } from "../controllers/auth/me.controller";
-
-import { logoutController } from "../controllers/auth/logout.controller";
 import AuthController from "../controllers/auth/auth.controller";
 
 const router = Router();
--- backend/src/routes/login.routes.ts
+++ backend/src/routes/login.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validate.middleware";
 import { userloginSchema } from "../validators/auth/login.validator";
-import { loginController } from "../controllers/auth/login.controller";
+import AuthController from "../controllers/auth/auth.controller";
 
 const router = Router();
 
-router.post("/login", validate(userloginSchema), loginController);
+router.post(
+  "/login",
+  validate(userloginSchema),
+  AuthController.loginController,
+);
 
 export default router;
