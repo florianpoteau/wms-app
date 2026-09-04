2ef3e79b3688e95bdef736058fe1935ad3df4a85
Poteau Florian <florianpoteau59@gmail.com>
2026-09-04 18:14:35 +0200

    refactor (error): simplifies error handling of middleware

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,49 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
+import AppError from "../error/AppError.middleware";
 
 export const errorHandling = (
-  error: Error,
+  error: AppError,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  if (error instanceof Error && error.message === "REFERENCE_ALREADY_EXISTS") {
-    return res.status(409).json({
-      message: "Il y a déja une référence sur cette article",
-    });
-  }
-  if (error instanceof Error && error.message === "BARCODE_ALREADY_EXISTS") {
-    return res.status(409).json({
-      message: "Il y a déja un barcode sur cette article",
-    });
-  }
-  if (error instanceof Error && error.message === "ARTICLE_NOT_FOUND") {
-    return res.status(404).json({
-      message: "Cette article n'existe pas",
-    });
-  }
-  if (error instanceof Error && error.message === "INVALID_CREDENTIALS") {
-    return res.status(400).json({
-      message: "Erreur de la connexion",
-    });
-  }
-  if (error instanceof Error && error.message === "JWT_NOT_DEFINED") {
-    return res.status(400).json({
-      message: "Veuillez vous reconnectez",
-    });
-  }
-  if (error instanceof Error && error.message === "USER_NOT_FOUND") {
-    return res.status(404).json({
-      message: "Utilisateur non trouvé",
-    });
-  }
-  if (error instanceof Error && error.message === "EMAIL_ALREADY_EXISTS") {
-    return res.status(409).json({
-      message: "Cette email existe déja",
-    });
-  }
-  if (error instanceof Error && error.message === "PHONE_ALREADY_EXISTS") {
-    return res.status(409).json({
-      message: "Ce numéro de téléphone est déja utilisé",
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
     });
   }
 
