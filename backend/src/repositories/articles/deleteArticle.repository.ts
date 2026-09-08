cebee088252795c2b174fd19a07016bde5f7d36f
Poteau Florian <florianpoteau59@gmail.com>
2026-09-08 17:32:17 +0200

    fix(delete): handle inactive products on delete

--- backend/src/repositories/articles/deleteArticle.repository.ts
+++ backend/src/repositories/articles/deleteArticle.repository.ts
@@ -1,9 +1,13 @@
 import { prisma } from "../../lib/prisma";
 
 export const deleteArticle = async (articleId: string) => {
-  return prisma.product.delete({
+  return prisma.product.update({
     where: {
       id: articleId,
+    },
+    data: {
+      active: false,
+      deletedAt: new Date(),
     },
   });
 };
