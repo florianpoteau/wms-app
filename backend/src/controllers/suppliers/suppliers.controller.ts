d7fe8c4ea59d9e3531883779fa7c5c471b509a3b
Poteau Florian <florianpoteau59@gmail.com>
2026-09-09 17:02:12 +0200

    feat(supplier): add update supplier controller and routes

--- backend/src/controllers/suppliers/suppliers.controller.ts
+++ backend/src/controllers/suppliers/suppliers.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { createSupplierService } from "../../services/supplier/createSupplier.service";
 import { getAllSupplierService } from "../../services/supplier/getAllSupplier.service";
 import { getSupplierByIdService } from "../../services/supplier/getSupplier.service";
+import { updateSupplierService } from "../../services/supplier/updateSupplier.service";
 
 export default class SupplierController {
   static getAllSupplierController = async (req: Request, res: Response) => {
@@ -20,5 +21,13 @@ export default class SupplierController {
     const data = res.locals.validated.params;
     const supplier = await getSupplierByIdService(data.id);
     return res.status(200).json(supplier);
+  };
+
+  static updateSupplierByIdController = async (req: Request, res: Response) => {
+    const data = res.locals.validated;
+    console.log(data);
+
+    await updateSupplierService(data.params.id, data.body);
+    return res.status(200).send();
   };
 }
