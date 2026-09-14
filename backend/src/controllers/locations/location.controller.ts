1a5e5bd43222158a199eae3e6f95cb21e093c3e4
Poteau Florian <florianpoteau59@gmail.com>
2026-09-14 16:12:34 +0200

    feat(location): add update location controller

--- backend/src/controllers/locations/location.controller.ts
+++ backend/src/controllers/locations/location.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { getAllLocationService } from "../../services/locations/getAllLocation.service";
 import { getLocationByIdService } from "../../services/locations/getLocationById.service";
 import { createLocationService } from "../../services/locations/createLocation.service";
+import { updateLocationService } from "../../services/locations/updateLocation.service";
 
 export default class LocationController {
   static getAllLocationController = async (req: Request, res: Response) => {
@@ -15,9 +16,15 @@ export default class LocationController {
     return res.status(200).json(location);
   };
 
-  static createLocationRepository = async (req: Request, res: Response) => {
+  static createLocationController = async (req: Request, res: Response) => {
     const data = res.locals.validated;
     await createLocationService(data.body);
     return res.status(201).send();
+  };
+
+  static updateLocationController = async (req: Request, res: Response) => {
+    const data = res.locals.validated;
+    await updateLocationService(data.params.id, data.body);
+    return res.status(200).send();
   };
 }
