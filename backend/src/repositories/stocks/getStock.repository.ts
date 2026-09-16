4e0ea62d0a4dd981986460e781695a7fc4daf3fc
Poteau Florian <florianpoteau59@gmail.com>
2026-09-16 14:12:25 +0200

    feat(stock): add getAllStock repository with filters

--- backend/src/repositories/stocks/getStock.repository.ts
+++ backend/src/repositories/stocks/getStock.repository.ts
@@ -1,20 +1,68 @@
 import { prisma } from "../../lib/prisma";
 
-export const getAllStockRepository = async () => {
-  return prisma.stock.findMany({
-    select: {
-      id: true,
-      quantity: true,
-      location: {
-        select: {
-          code: true,
+import type { GetAllStockInput } from "../../validators/stocks/stock.validator";
+
+export const getAllStockRepository = async (data: GetAllStockInput) => {
+  const { article, location, lowStock } = data;
+
+  const where = {
+    ...(article
+      ? {
+          product: {
+            reference: {
+              contains: article,
+              mode: "insensitive" as const,
+            },
+          },
+        }
+      : {}),
+
+    ...(location
+      ? {
+          location: {
+            code: {
+              startsWith: location,
+              mode: "insensitive" as const,
+            },
+          },
+        }
+      : {}),
+
+    ...(lowStock
+      ? {
+          quantity: {
+            lte: 20,
+          },
+        }
+      : {}),
+  };
+
+  const [stocks, totalStocks] = await prisma.$transaction([
+    prisma.stock.findMany({
+      where,
+      select: {
+        id: true,
+        quantity: true,
+        location: {
+          select: {
+            code: true,
+          },
+        },
+        product: {
+          select: {
+            reference: true,
+          },
         },
       },
-      product: {
-        select: {
-          reference: true,
-        },
-      },
-    },
-  });
+    }),
+
+    prisma.stock.count({
+      where,
+    }),
+  ]);
+
+  return {
+    stocks,
+    totalStocks,
+  };
 };
